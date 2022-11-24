3d021d94e7fd76c45614cb4ccb9be13291c5c34c
yykzjh <1378453948@qq.com>
2022-11-24 17:06:26 +0800

    init PolarizedSelfAttention

---
+++ MyAttentionModels/PolarizedSelfAttention2d.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+"""
+@author   :   yykzjh    
+@Contact  :   yykzhjh@163.com
+@DateTime :   2022/11/24 16:10
+@Version  :   1.0
+@License  :   (C)Copyright 2022
+"""
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MyAttentionModels/PolarizedSelfAttention3d.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+@author   :   yykzjh    
+@Contact  :   yykzhjh@163.com
+@DateTime :   2022/11/24 16:10
+@Version  :   1.0
+@License  :   (C)Copyright 2022
+"""
